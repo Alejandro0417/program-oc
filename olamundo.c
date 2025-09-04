507ae1e955294e4314453d62512d428098b3d399
Alejandro0417 <alejandroalmeida041.as@gmail.com>
2025-09-04 01:43:48 +0000

    criei super trunfo

--- olamundo.c
+++ olamundo.c
@@ -1,1 +1,19 @@
+#include <stdio.h> 
 
+int main() {
+int idade = 25;
+float altura = 1.75;
+char opção = 's';
+char nome[20] = "sergio";
+ 
+printf("digite sua idade: ");
+scanf("%d", &idade);
+printf("a idade é: %d\n", idade);
+
+printf("digite a sua altura: ");
+scanf("%f", &altura);
+
+
+    
+printf("a altura é: %f\n, altura");
+}
---
+++ super trunfo.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+
+int main(){
+char estado1 [20] = ceara;
+char cod carta1 = [a01];
+char nome da cidade1 = fortaleza[20];
+int população1 =  2.428.000;
+float área1 em km = 313.8 ;
+float pib1 = 73000000000000 ;
+int numero de pontos turisticos1 = 6 ;
+
+char estado2 [20] = "rio de janeiro";
+char cod carta2 [a02];
+char nome da cidade2 [20] "rio de janeiro";
+int população2 = 17.217.000
+float área2em km =1200;
+float pib2 = 950000000000;
+int numero de pontos turisticos2 = 58;
+
+printf("digite seu estado1: \n");
+scanf ("%d", &estado1);
+
+printf("digite seu cod carta1: \n");
+scanf("%d", carta1);
+
+printf("digite nome da cidade1: \n");
+scanf("%s", cidade1);
+
+printf("digites sua população1: \n");
+scanf("%e", população1);
+
+printf("digite sua área1 em km: \n");
+scanf("%f", área1);
+
+printf("digite seu pib1: \n");
+scanf("%f", pib1 );
+
+printf("digite numero de pontos turisticos1: \n");
+scanf("%d", turistico1);
+
+
+
+}
+ 
